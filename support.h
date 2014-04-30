63f53a1f74001469378d090e5a2f5d3f98f1ee47
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2014-04-30 11:39:31 +0200

    fix wrong gtk version for gtk_dialog_get_action_area

--- support.h
+++ support.h
@@ -1,6 +1,6 @@
 #include <gtk/gtk.h>
 
-#if !GTK_CHECK_VERSION(2,12,0)
+#if !GTK_CHECK_VERSION(2,14,0)
 #define gtk_widget_get_window(widget) ((widget)->window)
 #define gtk_dialog_get_content_area(dialog) (dialog->vbox)
 #define gtk_dialog_get_action_area(dialog) (dialog->action_area)
