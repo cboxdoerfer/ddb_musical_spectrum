a13e1c82cd78a76e9e6aee6fc941e0c3897dc40e
mwgg <m@mw.gg>
2015-01-15 10:21:17 +0300

    Number of bars now adjustable from plugin config window

--- spectrum.h
+++ spectrum.h
@@ -30,7 +30,9 @@
 #include <deadbeef/deadbeef.h>
 #include <deadbeef/gtkui_api.h>
 
-#define MAX_BANDS 126
+#include "config.h"
+
+#define MAX_BARS 2000
 #define REFRESH_INTERVAL 25
 #define GRADIENT_TABLE_SIZE 1024
 #define MAX_FFT_SIZE 32768
@@ -51,9 +53,9 @@ typedef struct {
     double *data;
     double window[MAX_FFT_SIZE];
     // keys: index of frequencies of musical notes (c0;d0;...;f10) in data
-    int keys[MAX_BANDS + 1];
+    int keys[MAX_BARS + 1];
     // freq: hold frequency values
-    float freq[MAX_BANDS + 1];
+    float freq[MAX_BARS + 1];
     uint32_t colors[GRADIENT_TABLE_SIZE];
     int samplerate;
     double *samples;
@@ -62,10 +64,10 @@ typedef struct {
     fftw_plan p_r2c;
     int buffered;
     int low_res_end;
-    float bars[MAX_BANDS + 1];
-    int delay[MAX_BANDS + 1];
-    float peaks[MAX_BANDS + 1];
-    int delay_peak[MAX_BANDS + 1];
+    float bars[MAX_BARS + 1];
+    int delay[MAX_BARS + 1];
+    float peaks[MAX_BARS + 1];
+    int delay_peak[MAX_BARS + 1];
     intptr_t mutex;
 } w_spectrum_t;
 
--- utils.c
+++ utils.c
@@ -35,17 +35,17 @@
 #include "spectrum.h"
 
 char *notes[] = {"C0","C#0","D0","D#0","E0","F0","F#0","G0","G#0","A0","A#0","B0",
-                        "C1","C#1","D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1",
-                        "C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2",
-                        "C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3",
-                        "C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4",
-                        "C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5",
-                        "C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6",
-                        "C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7",
-                        "C8","C#8","D8","D#8","E8","F8","F#8","G8","G#8","A8","A#8","B8",
-                        "C9","C#9","D9","D#9","E9","F9","F#9","G9","G#9","A9","A#9","B9",
-                        "C10","C#10","D10","D#10","E10","F10","F#10","G10","G#10","A10","A#10","B10"
-                       };
+                    "C1","C#1","D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1",
+                    "C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2",
+                    "C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3",
+                    "C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4",
+                    "C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5",
+                    "C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6",
+                    "C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7",
+                    "C8","C#8","D8","D#8","E8","F8","F#8","G8","G#8","A8","A#8","B8",
+                    "C9","C#9","D9","D#9","E9","F9","F#9","G9","G#9","A9","A#9","B9",
+                    "C10","C#10","D10","D#10","E10","F10","F#10","G10","G#10","A10","A#10","B10"
+                };
 
 void
 _memset_pattern (char *data, const void* pattern, size_t data_len, size_t pattern_len)
@@ -138,14 +138,18 @@ create_frequency_table (gpointer user_data)
     w_spectrum_t *w = user_data;
 
     w->low_res_end = 0;
-    for (int i = 0; i < MAX_BANDS; i++) {
-        w->freq[i] = 440.0 * pow (2.0, (double)(i-57)/12.0);
+
+    double ratio = CONFIG_NUM_BARS / 126.0;
+    double a4pos = 57.0 * ratio;
+    double octave = 12.0 * ratio;
+
+    for (int i = 0; i < CONFIG_NUM_BARS; i++) {
+        w->freq[i] = 440.0 * pow (2.0, (double)(i-a4pos)/octave);
         w->keys[i] = ftoi (w->freq[i] * CONFIG_FFT_SIZE/(float)w->samplerate);
         if (i > 0 && w->keys[i-1] == w->keys[i])
             w->low_res_end = i;
     }
 }
-
 float
 linear_interpolate (float y1, float y2, float mu)
 {
@@ -161,4 +165,3 @@ lagrange_interpolate (float y0, float y1, float y2, float y3, float x)
     const float a3 = ((x - 0) * (x - 1) * (x - 2)) /  6 * y3;
     return (a0 + a1 + a2 + a3);
 }
-
