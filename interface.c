c79649bddf883bb450a803d2c550837af8b85514
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2019-11-30 13:08:38 +0100

    config_dialog: set title name

--- interface.c
+++ interface.c
@@ -164,7 +164,7 @@ create_config_dialog (void)
   GtkWidget *okbutton1;
 
   config_dialog = gtk_dialog_new ();
-  gtk_window_set_title (GTK_WINDOW (config_dialog), _("dialog1"));
+  gtk_window_set_title (GTK_WINDOW (config_dialog), _("Spectrum options"));
   gtk_window_set_type_hint (GTK_WINDOW (config_dialog), GDK_WINDOW_TYPE_HINT_DIALOG);
   gtk_dialog_set_has_separator (GTK_DIALOG (config_dialog), FALSE);
 
